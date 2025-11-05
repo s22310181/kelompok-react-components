833f9ef5faad0de03437ee90415f4172e539d40a
carolina <s22310181@student.unklab.ac.id>
2025-11-05 21:27:08 +0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import Header from "./components/Header";
-import Content from "./components/Content/Content";
-import Footer from "./components/Footer";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
-      <Header title="Selamat Datang di Website Kami" />
-      <Content />
-      <Footer text="© 2025 Kelompok React" />
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
